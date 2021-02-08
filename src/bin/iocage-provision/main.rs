6c84a5a585cb96cddc61fa7f48da3da922344f00
Fletcher Nichol <fnichol@nichol.ca>
2021-02-08 00:03:34 -0700

    chore: update project dependencies & structure
    There are several changes here, including:
    
    * swapping from structopt to clap 3 beta
    * swapping from failure to thiserror & anyhow
    * better splitting of library and cli dependencies
    * improving error type hierarchy
    * updating dependencies
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- src/bin/iocage-provision/main.rs
+++ src/bin/iocage-provision/main.rs
@@ -2,26 +2,16 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use iocage_provision::Result;
-use log::{debug, error};
-use std::process;
+use anyhow::Result;
+use log::debug;
 
 mod cli;
 
-fn main() {
+fn main() -> Result<()> {
     cli::util::setup_panic_hooks();
 
-    if let Err(err) = try_main() {
-        for line in cli::util::pretty_error(&err).lines() {
-            error!("{}", line);
-        }
-        process::exit(1);
-    }
-}
-
-fn try_main() -> Result<()> {
-    let args = cli::from_args();
-    cli::util::init_logger(args.verbose);
+    let args = cli::parse();
+    cli::util::init_logger_with_verbosity(args.verbose);
     debug!("parsed cli arguments; args={:?}", args);
 
     iocage_provision::ensure_root()?;
--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use failure::{Fail, ResultExt};
 use ipnet::IpNet;
 use log::{debug, info, warn};
-use std::error;
+use nix::sys::utsname;
 use std::ffi::OsStr;
-use std::fmt;
 use std::fs;
-use std::io::{BufRead, BufReader, Write};
-use std::net::IpAddr;
+use std::io::{self, BufRead, BufReader, Write};
+use std::net::{self, IpAddr};
 use std::path::Path;
 use std::process::{ChildStdin, Command, ExitStatus, Stdio};
 use std::result;
+use std::str;
 use std::thread;
 use tempfile::NamedTempFile;
 use users::{os::unix::UserExt, Group, User};
@@ -29,41 +28,72 @@ macro_rules! section {
 }
 
 /// A specialized `Result` type for this crate's operations.
-pub type Result<T> = result::Result<T, failure::Error>;
+pub type Result<T> = result::Result<T, Error>;
 
 /// Error type for this crate.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
-    /// A command returned a non-zero exit code and thus is considered to have failed.
-    CmdFail(i32),
-    /// A command I/O error for one of the streams.
-    CmdIO,
-    /// A command-related thread panicked.
-    CmdThread,
+    #[error("could not generate json pkglist tempfile")]
+    CreatePkglistJson(#[source] io::Error),
+    #[error("failed to create user group")]
+    ExecCreateGroup(#[source] IocageExecError),
+    #[error("failed to create user")]
+    ExecCreateUser(#[source] IocageExecError),
+    #[error("failed to enable an SSH service")]
+    ExecSshService(#[source] IocageExecError),
+    #[error("failed to prepare sudo config")]
+    ExecSudoConfig(#[source] IocageExecError),
+    #[error("failed to create iocage jail")]
+    IocageCreate(#[source] CmdError),
     /// A system group ID was not found.
+    #[error("system group id not found; gid={0}")]
     NoGid(u32),
-    /// A system user name was not found.
-    NoUser(String),
     /// The effective user is not currently the `root` user.
+    #[error("root privileges required")]
     NotRoot,
+    /// A system user name was not found.
+    #[error("system user not found; user={0}")]
+    NoUser(String),
 }
 
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use Error::*;
-
-        match self {
-            CmdFail(ref code) => write!(f, "command exited with code: {}", code),
-            CmdIO => f.write_str("command i/o error"),
-            CmdThread => f.write_str("command i/o thread error"),
-            NoGid(ref group) => write!(f, "system group id not found: {}", group),
-            NoUser(ref user) => write!(f, "system user not found: {}", user),
-            NotRoot => f.write_str("root privileges required"),
-        }
-    }
+#[derive(Debug, thiserror::Error)]
+pub enum CmdError {
+    #[error("spawned command did not start")]
+    ChildWait(#[source] io::Error),
+    /// A command returned a non-zero exit code and thus is considered to have failed.
+    #[error("command exited with non-zero code; code={0}")]
+    Failed(i32),
+    #[error("command failed to spawn; program={0}")]
+    Spawn(String, #[source] io::Error),
+    #[error("stream was not captured; stream={0}")]
+    StreamCapture(&'static str),
+    #[error("io stream thread panicked; stream={0}")]
+    Thread(&'static str),
+    #[error("failed to write to stdin")]
+    StdinWrite(#[source] io::Error),
 }
 
-impl error::Error for Error {}
+/// Error when an iocage exec command fails.
+#[derive(Debug, thiserror::Error)]
+#[error("iocage exec command failed")]
+pub struct IocageExecError(#[from] CmdError);
+
+/// Error when determining a default gateway IP address.
+#[derive(Debug, thiserror::Error)]
+pub enum GatewayError {
+    /// A command cannot be found or run successfully.
+    #[error("failed to successfully run netstat command; err={0}")]
+    Cmd(#[source] io::Error),
+    /// An IP address failed to be parsed.
+    #[error("failed to parse ip address")]
+    IpAddr(#[source] net::AddrParseError),
+    /// The output of the `netstat` command failed to be parsed.
+    #[error("failed to parse netstat output; cause={0}")]
+    NetstatParse(&'static str),
+    /// A string failed to be parsed as UTF-8.
+    #[error("utf8 error; err={0}")]
+    Utf8(#[source] str::Utf8Error),
+}
 
 /// Ensures that the current effective user is root.
 ///
@@ -72,7 +102,7 @@ impl error::Error for Error {}
 /// Returns an `Err` if the current effective `uid` is any value other than `0`.
 pub fn ensure_root() -> Result<()> {
     if users::get_effective_uid() != 0 {
-        Err(Error::NotRoot.into())
+        Err(Error::NotRoot)
     } else {
         Ok(())
     }
@@ -94,35 +124,78 @@ pub fn provision_jail(
     ssh_service: bool,
 ) -> Result<()> {
     let user = find_user(user)?;
-    let json =
-        create_pkglist_json(user.as_ref()).context("could not generate json pkglist tempfile")?;
+    let json = create_pkglist_json(user.as_ref()).map_err(Error::CreatePkglistJson)?;
 
     section!("Provisioning a jail named '{}'", name);
 
     info!("Creating '{}' via iocage", name);
-    run_iocage_create(name, ip, gateway, release, json.path()).context("failed to create jail")?;
+    run_iocage_create(name, ip, gateway, release, json.path())?;
 
     if let Some(user) = user {
         let group = find_group(user.primary_group_id())?;
 
         info!("Preparing sudo config");
-        exec_sudo_config(name).context("failed to prepare sudo config")?;
+        exec_sudo_config(name)?;
 
         info!("Creating group '{}'", group.name().to_string_lossy());
-        exec_create_group(name, &group).context("failed to create group")?;
+        exec_create_group(name, &group)?;
 
         info!("Creating user '{}'", user.name().to_string_lossy());
-        exec_create_user(name, &user, &group).context("failed to create user")?;
+        exec_create_user(name, &user, &group)?;
     }
 
     if ssh_service {
         info!("Enabling SSH service");
-        exec_ssh_service(name).context("failed to enable SSH service")?;
+        exec_ssh_service(name)?;
     }
 
     section!("Instance '{}' provisioned successfully", name);
 
     Ok(())
+}
+
+/// Determines and returns a default gateway IP address by querying the `netstat` command.
+///
+/// # Errors
+///
+/// Returns an `Err` if:
+///
+/// * The `netstat` command cannot be found
+/// * The output of the command cannot be parsed as UTF-8
+/// * No line of output starting with `"default"` can be found
+/// * The default line cannot be successfully split
+/// * The IP address string cannot be parsed as an IP address
+pub fn netstat_gateway_addr() -> result::Result<IpAddr, GatewayError> {
+    str::from_utf8(
+        Command::new("netstat")
+            .args(&["-r", "-n", "-f", "inet"])
+            .output()
+            .map_err(GatewayError::Cmd)?
+            .stdout
+            .as_ref(),
+    )
+    .map_err(GatewayError::Utf8)?
+    .lines()
+    .find(|line| line.starts_with("default"))
+    .map(|line| line.split_ascii_whitespace())
+    .ok_or(GatewayError::NetstatParse("default line not found"))?
+    .nth(1)
+    .ok_or(GatewayError::NetstatParse(
+        "second column not found on default line",
+    ))?
+    .parse()
+    .map_err(GatewayError::IpAddr)
+}
+
+/// Returns a default release value based on the current host.
+pub fn default_release() -> String {
+    utsname::uname()
+        .release()
+        .split('-')
+        .map(|s| if s == "STABLE" { "RELEASE" } else { s })
+        .take(2)
+        .collect::<Vec<_>>()
+        .join("-")
 }
 
 /// Returns a `User` for a given name, if one exists.
@@ -136,7 +209,7 @@ fn find_user(user_str: Option<&str>) -> Result<Option<User>> {
     match user_str {
         Some(user_str) => match users::get_user_by_name(user_str) {
             Some(user_info) => Ok(Some(user_info)),
-            None => Err(Error::NoUser(user_str.to_string()).into()),
+            None => Err(Error::NoUser(user_str.to_string())),
         },
         None => Ok(None),
     }
@@ -148,7 +221,7 @@ fn find_user(user_str: Option<&str>) -> Result<Option<User>> {
 ///
 /// Returns an `Err` if an associated system group cannot be found for the given group ID.
 fn find_group(gid: u32) -> Result<Group> {
-    users::get_group_by_gid(gid).ok_or_else(|| Error::NoGid(gid).into())
+    users::get_group_by_gid(gid).ok_or(Error::NoGid(gid))
 }
 
 /// Creates a package list JSON file for the `iocage create` subcommand and returns the file path.
@@ -156,7 +229,7 @@ fn find_group(gid: u32) -> Result<Group> {
 /// # Errors
 ///
 /// Returns an `Err` if the JSON file could not be successfully created and written.
-fn create_pkglist_json(user: Option<&User>) -> Result<NamedTempFile> {
+fn create_pkglist_json(user: Option<&User>) -> io::Result<NamedTempFile> {
     let json_str = match user {
         Some(user) => {
             let shell = user
@@ -193,6 +266,7 @@ fn exec_sudo_config(jail_name: &str) -> Result<()> {
         jail_name,
         "echo '%wheel ALL=(ALL) NOPASSWD: ALL' >/usr/local/etc/sudoers.d/wheel",
     )
+    .map_err(Error::ExecSudoConfig)
 }
 
 /// Creates a system group in the given jail.
@@ -209,6 +283,7 @@ fn exec_create_group(jail_name: &str, group: &Group) -> Result<()> {
             grp = group.name().to_string_lossy(),
         ),
     )
+    .map_err(Error::ExecCreateGroup)
 }
 
 /// Creates a system user in the given jail.
@@ -227,6 +302,7 @@ fn exec_create_user(jail_name: &str, user: &User, group: &Group) -> Result<()> {
             usr = user.name().to_string_lossy(),
         ),
     )
+    .map_err(Error::ExecCreateUser)
 }
 
 /// Configures and starts an SSH service in the given jail.
@@ -239,6 +315,7 @@ fn exec_ssh_service(jail_name: &str) -> Result<()> {
         jail_name,
         r#"sysrc -f /etc/rc.conf sshd_enable="YES" && service sshd start"#,
     )
+    .map_err(Error::ExecSshService)
 }
 
 /// Creates a new jail with the given configuration.
@@ -269,14 +346,14 @@ fn run_iocage_create(
         .arg("boot=on")
         .env("PYTHONUNBUFFERED", "true");
 
-    let status = spawn_and_indent(cmd).context("failed to run: iocage create")?;
+    let status = spawn_and_indent(cmd).map_err(Error::IocageCreate)?;
 
     if status.success() {
         Ok(())
     } else {
-        Err(Error::CmdFail(status.code().unwrap_or(-1))
-            .context("iocage create command failed")
-            .into())
+        Err(Error::IocageCreate(CmdError::Failed(
+            status.code().unwrap_or(-1),
+        )))
     }
 }
 
@@ -289,7 +366,7 @@ fn run_iocage_create(
 /// * The input and output streams were not successfully set up
 /// * The `iocage` program was not found
 /// * The `iocage` exits with a code that is not zero
-fn iocage_exec<S: AsRef<str>>(jail_name: &str, src: S) -> Result<()> {
+fn iocage_exec<S: AsRef<str>>(jail_name: &str, src: S) -> result::Result<(), IocageExecError> {
     let mut cmd = Command::new("iocage");
     cmd.arg("exec")
         .arg(jail_name)
@@ -304,20 +381,17 @@ fn iocage_exec<S: AsRef<str>>(jail_name: &str, src: S) -> Result<()> {
     let status = spawn_and_indent_with_stdin(cmd, |mut stdin| {
         stdin
             .write_all(b"set -eu\n\n")
-            .context("failed to write to stdin")?;
+            .map_err(CmdError::StdinWrite)?;
         stdin
             .write_all(src.as_ref().as_bytes())
-            .context("failed to write to stdin")?;
+            .map_err(CmdError::StdinWrite)?;
         Ok(())
-    })
-    .context("failed to run: iocage exec")?;
+    })?;
 
     if status.success() {
         Ok(())
     } else {
-        Err(Error::CmdFail(status.code().unwrap_or(-1))
-            .context("iocage exec command failed")
-            .into())
+        Err(CmdError::Failed(status.code().unwrap_or(-1)).into())
     }
 }
 
@@ -331,7 +405,7 @@ fn iocage_exec<S: AsRef<str>>(jail_name: &str, src: S) -> Result<()> {
 /// * One of the I/O streams failed to be properly captured
 /// * One of the output-reading threads panics
 /// * The command wasn't running
-fn spawn_and_indent(cmd: Command) -> Result<ExitStatus> {
+fn spawn_and_indent(cmd: Command) -> result::Result<ExitStatus, CmdError> {
     spawn_and_indent_with_stdin(cmd, |_| Ok(()))
 }
 
@@ -346,21 +420,24 @@ fn spawn_and_indent(cmd: Command) -> Result<ExitStatus> {
 /// * One of the I/O streams failed to be properly captured
 /// * One of the output-reading threads panics
 /// * The command wasn't running
-fn spawn_and_indent_with_stdin<F>(mut cmd: Command, stdin_func: F) -> Result<ExitStatus>
+fn spawn_and_indent_with_stdin<F>(
+    mut cmd: Command,
+    stdin_func: F,
+) -> result::Result<ExitStatus, CmdError>
 where
-    F: FnOnce(ChildStdin) -> Result<()>,
+    F: FnOnce(ChildStdin) -> result::Result<(), CmdError>,
 {
     cmd.stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped());
 
     debug!("running; cmd={:?}", &cmd);
-    let mut child = cmd.spawn().context("command failed to spawn")?;
+    let mut child = cmd
+        .spawn()
+        .map_err(|err| CmdError::Spawn(cmd_get_program(&cmd), err))?;
+
     {
-        let stdin = child
-            .stdin
-            .take()
-            .ok_or_else(|| Error::CmdIO.context("stdin was not captured"))?;
+        let stdin = child.stdin.take().ok_or(CmdError::StreamCapture("stdin"))?;
         stdin_func(stdin)?;
     }
 
@@ -368,7 +445,7 @@ where
         child
             .stdout
             .take()
-            .ok_or_else(|| Error::CmdIO.context("stdout was not captured"))?,
+            .ok_or(CmdError::StreamCapture("stdout"))?,
     );
     let stdout_handle = thread::spawn(move || {
         for line in stdout.lines() {
@@ -387,7 +464,7 @@ where
         child
             .stderr
             .take()
-            .ok_or_else(|| Error::CmdIO.context("stderr was not captured"))?,
+            .ok_or(CmdError::StreamCapture("stderr"))?,
     );
     let stderr_handle = thread::spawn(move || {
         for line in stderr.lines() {
@@ -406,10 +483,18 @@ where
 
     stdout_handle
         .join()
-        .map_err(|_| Error::CmdThread.context("stdout thread panicked"))?;
+        .map_err(|_| CmdError::Thread("stdout"))?;
     stderr_handle
         .join()
-        .map_err(|_| Error::CmdThread.context("stderr thread panicked"))?;
+        .map_err(|_| CmdError::Thread("stderr"))?;
 
-    Ok(status.context("command did not run")?)
+    status.map_err(CmdError::ChildWait)
+}
+
+fn cmd_get_program(cmd: &Command) -> String {
+    shell_words::split(&format!("{:?}", cmd))
+        .ok()
+        .map(|args| args.into_iter().next())
+        .flatten()
+        .unwrap_or_else(|| "<unknown>".to_string())
 }
