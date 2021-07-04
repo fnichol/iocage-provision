f6f05c29194870ef1d33af8def6c599def610284
Fletcher Nichol <fnichol@nichol.ca>
2021-07-04 12:33:57 -0600

    release: iocage-provision 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#![doc(html_root_url = "https://docs.rs/iocage-provision/0.1.2-dev")]
+#![doc(html_root_url = "https://docs.rs/iocage-provision/0.2.0")]
 //#![deny(missing_docs)]
 
 use ipnet::IpNet;
